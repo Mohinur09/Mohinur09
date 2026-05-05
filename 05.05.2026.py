4ae3a7b87a85cc0d77e0d25cd86ab189f0ce4aa1
Ro'zmetova Mohinur <rozmetovamohinur2@gmail.com>
2026-05-05 17:55:32 +0500

    1

---
+++ 05.05.2026.py
@@ -0,0 +1,49 @@
+#  class xodimlar:
+#     def __init__(self,ism,lavozim,tajriba,maosh):
+#         self.name = ism
+#         self.lavozim = lavozim
+#         self.tajriba = tajriba
+#         self.maosh = maosh
+#     def get_name(self):
+#         name = f"{self.name.title()}"
+#         return name
+
+#     def get_lavozim(self):
+#         return f"{self.lavozim.title()}"
+
+#     def get_maosh(self):
+#         return f"{self.maosh.title()}"
+
+#     def get_info(self):
+#         info = f"{self.name.title()} {self.experens} yillik tajribaga ega {self.lavozim.title}
+#         return info 
+# # hodimm1 = hodimlar('kamolbek','tadbirkor','3','12000000')
+# # print(hodimm1.get_lavozim())
+
+
+# class Magazin(xodimlar):
+#     def __init__(self,ism,lavozim,tajriba,maosh,maxsulot_turi,m_nomi):
+#         super().__init__(ism,lavozim,tajriba,maosh)
+#         self.maxsulot_turi = maxsulot_turi
+#     def get_maxsulot_turi(self):
+#         return f"bu magazin {self.maxsulot_turi} uchun moljallangan. {self.name.title()} {self.lavozim} lavozimida {self.maosh} som oladi, {self.experens} ga ega"
+        
+    
+        
+        
+# magazin1 = Magazin('ixtiyor','boshqaruvchi',1.5,10000000,'oziq ovqat')
+# print(magazin)
+
+
+
+
+
+
+
+
+class project_pupils:
+    def __init__(self, loyiha_nomi, loyihadagi_9sinf_oquvchilari,):
+        self.loyiha_nomi = loyiha_nomi
+        self.loyihadagi_9sinf_oquvchilari = loyihadagi_9sinf_oquvchilari
+        
+
